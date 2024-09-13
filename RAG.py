bbf850c443e7fe4b051ebbb2f039fbf0520634bc
SalehAhmad1 <salehahmad2106@gmail.com>
2024-09-14 02:24:58 +0500

    miss

--- RAG.py
+++ RAG.py
@@ -1,14 +1,14 @@
+
 import os
 import shutil
-import numpy as np
-import pandas as pd
+import time
+import logging
 from dotenv import load_dotenv
 from git import Repo
 from langchain_milvus import Milvus
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import GitLoader
 from openai import OpenAI
-
 class GitHubGPT:
     def __init__(self):
         self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -16,9 +16,8 @@ class GitHubGPT:
         self.vector_db = self.__initialize_vector_db()
         self.client = OpenAI(api_key=self.OPENAI_API_KEY)
         self.system_prompt = self.__initialize_system_prompt()
-        self.thread = None
         self.thread_id = None
-        self.messages = []  # List to store messages
+        self.assistant_id = self.__create_assistant(name='Github GPT', instructions='Please address the user as Github GPT')
 
     def __initialize_embeddings(self):
         return OpenAIEmbeddings(
@@ -39,59 +38,24 @@ class GitHubGPT:
         
     def __initialize_system_prompt(self):
         return '''
-    What are you? A well informed, intelligent chatbot which can talk to a given codebase.
-    What do you do? You are always given some file content from a codebase and a question/prompt. Your job is to generate a response.
-    What should be the tone of your output? It should be friendly, helpful, confident, narrative.
+    What are you? A well-informed, intelligent chatbot that can interact with a codebase.
+    What do you do? You are always provided with some file content from a codebase and a question/prompt. Your job is to generate a response.
+    What should be the tone of your output? It should be friendly, helpful, confident, and narrative.
     What outputs can we expect from you? You can be asked to generate documentations, code, or anything else only relevant to the given codebase content.
     '''
-
+    
     @staticmethod
     def __clean_repo_name(name):
         return name.replace('-', '_')
-    
-    @staticmethod
-    def __declean_repo_name(name):
-        return name.replace('_', '-')
-    
-    def __add_repo_data_to_db(self):
-        data = self.loader.load()
-        print(f'Length of Data to Add: {len(data)}')
-        print(f'Adding Data to Milvus Vector DB')
-        self.vector_db.add_documents(documents=data)
-        print(f'Done Adding Data to Milvus Vector DB')
-    
-    def add_repo(self, repo_url):
-        repo_name = repo_url.split('/')[-1]
-        repo_save_path = f"./Data/Repos"
-        if not os.path.exists(repo_save_path):
-            os.makedirs(repo_save_path)
-        else:
-            shutil.rmtree(repo_save_path)
-            os.makedirs(repo_save_path)
-        repo_save_path = repo_save_path + "/" + self.__clean_repo_name(repo_name)
-        
-        print(f'Cloning the repo from: {repo_url}')
-        repo = Repo.clone_from(
-            repo_url, 
-            to_path=repo_save_path,
-            branch="master"
-        )
-        print(f'Repo Cloned to: {repo_save_path}')
-        self.repo_save_path = repo_save_path
-        self.branch = repo.head.reference
-        self.loader = GitLoader(repo_path=repo_save_path, branch=self.branch)
-        self.__add_repo_data_to_db()
 
-    def load_repo(self):
-        repo_save_path = "./Data/Repos"
-        repo_name = os.listdir(repo_save_path)[0]
-        self.repo_save_path = repo_save_path + "/" + repo_name
-        self.branch = "master"
-        print(f'Loading repo: {repo_name}')
-        print(f'Branch: {self.branch}')
-        print(f'Repo path: {self.repo_save_path}')
-        self.loader = GitLoader(repo_path=self.repo_save_path, branch=self.branch)
-        self.__add_repo_data_to_db()
+    def __create_assistant(self, name, instructions, model="gpt-3.5-turbo-16k"):
+        assistant = self.client.beta.assistants.create(
+            name=name,
+            instructions=instructions,
+            model=model,
+        )
+        print(f'Assistant created with ID: {assistant.id}')
+        return assistant.id
 
     def __retrieve_documents(self, prompt, k=3):
         retrieved_documents = self.vector_db.similarity_search(
@@ -109,35 +73,55 @@ class GitHubGPT:
             All_content += "Chunk:" + str(idx) + ":\n" + doc.page_content + "\n\n"
         print("\n\n")
         return All_content
-    
-    def query(self, prompt):
+
+    def query(self, prompt, instructions="Please address the user as Github User"):
+        # Step 1: Retrieve relevant documents based on the user's query
         retrieved_documents = self.__retrieve_documents(prompt)
         context = self.__concatenate_documents(retrieved_documents)
 
-        # Add the user message to the messages list
-        self.messages.append({"role": "user", "content": f"Context from codebase:{context}\nUser query prompt:{prompt}\nResponse:\n"})
-        
-        completion = self.client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": f"{self.system_prompt}"},
-            {"role": "user", "content": f"Context from codebase:{context}\nUser query prompt:{prompt}\nResponse:\n"},
-        ],
-        stream=True
+        # Step 2: Create a thread using the retrieved context and user prompt
+        user_query = f"Context from codebase: {context}\nUser query: {prompt}\n"
+        thread = self.client.beta.threads.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_query,
+                }
+            ]
         )
-        
-        response_text = ''
-        for chunk in completion:
-            if chunk.choices[0].delta.content is not None:
-                response_text += chunk.choices[0].delta.content
-                yield chunk.choices[0].delta.content
-        
-        self.messages.append({"role": "assistant", "content": response_text})
-        print(f'\n\nMessage has been added to the list of messages: {self.messages}, which will be later on a part of the retrieved thread. To retrieve the thread, call retrieve_thread()')
-    
-    def retrieve_thread(self):
-        thread = self.client.beta.threads.create(messages=self.messages)
         self.thread_id = thread.id
-        self.thread = thread
-        print(f'The thread ID is: {thread.id}')
-        return thread
+        print(f'Thread created with ID: {self.thread_id}')
+
+        # Step 3: Run the assistant on the created thread
+        run = self.client.beta.threads.runs.create(
+            thread_id=self.thread_id,
+            assistant_id=self.assistant_id,
+            instructions=instructions,
+        )
+
+        # Step 4: Wait for the assistant's response
+        self.wait_for_run_completion(self.client, thread_id=self.thread_id, run_id=run.id)
+
+    def wait_for_run_completion(self, client, thread_id, run_id, sleep_interval=5):
+        while True:
+            try:
+                run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+                if run.completed_at:
+                    elapsed_time = run.completed_at - run.created_at
+                    formatted_elapsed_time = time.strftime(
+                        "%H:%M:%S", time.gmtime(elapsed_time)
+                    )
+                    print(f"Run completed in {formatted_elapsed_time}")
+                    logging.info(f"Run completed in {formatted_elapsed_time}")
+                    # Get messages here once the run is completed!
+                    messages = client.beta.threads.messages.list(thread_id=thread_id)
+                    print(f'All messages: {messages.data}\n')
+                    last_message = messages.data[0]
+                    response = last_message.content[0].text.value
+                    print(f"Assistant Response: {response}\n\n****\n\n")
+                    break
+            except Exception as e:
+                logging.error(f"An error occurred while retrieving the run: {e}")
+                break
+            logging.info("Waiting for run to complete...")
+            time.sleep(sleep_interval)
